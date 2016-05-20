566b57f06d8184319c3f32427ba2f05b3e0fea64
xcjs <zlowery@xcjs.com>
2016-05-20 18:36:51 -0400

    Added distro support for Linux.

--- api/routes/system.js
+++ api/routes/system.js
@@ -1,8 +1,10 @@
 /*jslint node: true */
+/*jslint esversion: 6 */
 
 'use strict';
 
 var os = require('os');
+var exec = require('child_process').exec;
 
 module.exports = getRoutes();
 
@@ -13,15 +15,55 @@ function getRoutes() {
         method: 'GET',
         path: '/api/system',
         handler: function (request, reply) {
-            return reply({
+            var response = {
                 architecture: os.arch(),
                 host: os.hostname(),
                 platform: os.platform(),
                 release: os.release(),
                 uptime: os.uptime()
+            };
+
+            var promise = new Promise(function (resolve) {
+                getDistro().then(function (result) {
+                    response.distro = result;
+                    resolve(response);
+                });
             });
+
+            return reply(promise);
         }
     });
 
     return routes;
+}
+
+function getDistro() {
+    var promise = new Promise(function (resolve) {
+        // Unescaped command: cat /etc/lsb-release | grep "DISTRIB_DESCRIPTION" | sed -e 's/[^"]*"\([^"]*\)".*/\1/'
+        var command =
+            "cat /etc/lsb-release | grep \"DISTRIB_DESCRIPTION\" | sed -e 's/[^\"]*\"\\([^\"]*\\)\".*/\\1/'";
+
+        var cb = function (out) {
+            resolve(out);
+        };
+
+        shell(command, cb);
+    });
+
+    return promise;
+}
+
+function shell(command, callback) {
+    exec(command, function (error, stdout, stderr) {
+        var eolLength = os.EOL.length;
+        var finalNewLine = stdout.substr(
+            stdout.length - eolLength,
+            stdout.length);
+
+        if (finalNewLine === os.EOL) {
+            stdout = stdout.substr(0, stdout.length - eolLength);
+        }
+
+        callback(stdout, stderr, error);
+    });
 }
