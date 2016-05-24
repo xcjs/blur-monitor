5e91641b0199bd45cfa5ed9ffe9e835952a3dca5
xcjs <zlowery@xcjs.com>
2016-05-24 18:19:56 -0400

    Began serving static files through Hapi.

--- gulp/server.js
+++ gulp/server.js
@@ -6,6 +6,8 @@ var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
 
+var nodemon = require('gulp-nodemon');
+
 var browserSync = require('browser-sync');
 var browserSyncSpa = require('browser-sync-spa');
 
@@ -13,52 +15,32 @@ var util = require('util');
 
 var proxyMiddleware = require('http-proxy-middleware');
 
-function browserSyncInit(baseDir, browser) {
+function browserSyncInit(baseDir, browser, taskName) {
     browser = browser === undefined ? 'default' : browser;
 
-    var routes = null;
-    if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(
-            conf.paths.src) !== -1)) {
-        routes = {
-            '/bower_components': 'bower_components'
-        };
-    }
-
-    var server = {
-        baseDir: baseDir,
-        routes: routes
-    };
-
-    /*
-     * You can add a proxy to your backend by uncommenting the line below.
-     * You just have to configure a context which will we redirected and the target url.
-     * Example: $http.get('/users') requests will be automatically proxified.
-     *
-     * For more details and option, https://github.com/chimurai/http-proxy-middleware/blob/v0.9.0/README.md
-     */
-    // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', changeOrigin: true});
-
-    server.middleware = proxyMiddleware('/api', { target: 'http://localhost:3000/' });
-
-    browserSync.instance = browserSync.init({
-        startPath: '/',
-        server: server,
-        port: 8080,
-        browser: browser,
-        ghostMode: false
+    nodemon({
+        script: './api/server.js',
+        args: [taskName],
+        ext: 'js'
+    }).on('start', function() {
+        browserSync.instance = browserSync.init({
+            startPath: '/',
+            proxy: {
+              target: 'localhost:3000'
+            },
+            port: 8080,
+            browser: browser,
+            ghostMode: false
+        });
     });
 }
 
-browserSync.use(browserSyncSpa({
-    selector: '[ng-app]' // Only needed for angular apps
-}));
-
-gulp.task('serve', ['watch', 'api'], function () {
-    browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+gulp.task('serve', ['watch'], function () {
+    browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src], undefined, 'serve');
 });
 
-gulp.task('serve:dist', ['build', 'api:dist'], function () {
-    browserSyncInit(conf.paths.dist);
+gulp.task('serve:dist', ['build'], function () {
+    browserSyncInit(conf.paths.dist, undefined, 'serve:dist');
 });
 
 gulp.task('serve:e2e', ['inject'], function () {
