653da95a9c12b8dc667046e4f86aab9babdda937
xcjs <zlowery@xcjs.com>
2016-05-27 19:53:34 -0400

    Added a constant module to expose constats/settings application-wide.

---
+++ src/app/constants/blur-constants.module.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+
+    angular.module('BlurMonitor.constants', [])
+        // The time interval to call the various services in milliseconds.
+        .constant('refreshInterval', 1000);
+})();
