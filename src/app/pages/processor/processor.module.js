fbaea48964efead1badc1121602b317b5959e972
xcjs <zlowery@xcjs.com>
2016-06-03 18:49:06 -0400

    Removed unnecessary chart.js configuration.

--- src/app/pages/processor/processor.module.js
+++ src/app/pages/processor/processor.module.js
@@ -3,10 +3,9 @@
 
     angular.module('BlurMonitor.pages.processor', [
         'BlurMonitor.constants',
-        'chart.js',
         'ngResource'
     ])
-        .config(routeConfig, chartJsConfig);
+        .config(routeConfig);
 
     /** @ngInject */
     function routeConfig($stateProvider) {
@@ -23,21 +22,4 @@
                 }
             });
     }
-
-    function chartJsConfig(ChartJsProvider, baConfigProvider) {
-        var layoutColors = baConfigProvider.colors;
-        // Configure all charts
-        ChartJsProvider.setOptions({
-            colours: [ layoutColors.primary, layoutColors.danger, layoutColors.warning, layoutColors.success, layoutColors.info, layoutColors.default, layoutColors.primaryDark, layoutColors.successDark, layoutColors.warningLight, layoutColors.successLight, layoutColors.primaryLight],
-            responsive: true,
-            scaleFontColor: layoutColors.defaultText,
-            scaleLineColor: layoutColors.border,
-            pointLabelFontColor: layoutColors.defaultText
-        });
-        // Configure all line charts
-        ChartJsProvider.setOptions('Line', {
-            datasetFill: false
-        });
-    }
-
 })();
