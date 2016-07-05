007b54908aec20381d9ea6ae8c3533f6beb208df
xcjs <zlowery@xcjs.com>
2016-07-05 19:07:26 -0400

    Added external IP information to the service.

--- api/routes/network.js
+++ api/routes/network.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var os = require('os');
+var spawn = require("child_process").spawn;
 
 module.exports = getRoutes();
 
@@ -25,5 +26,30 @@ function getRoutes() {
         }
     });
 
+    routes.push({
+        method: 'GET',
+        path: '/api/network/external',
+        handler: function(request, reply) {
+            return reply(getExternalIp());
+        }
+    });
+
     return routes;
+}
+
+function getExternalIp() {
+    var dig = spawn('dig', ['+short', 'myip.opendns.com', '@resolver1.opendns.com']);
+    dig.stdout.setEncoding("utf8");
+
+    var promise = new Promise(function(resolve) {
+        dig.stdout.on('data', function(data) {
+            var external = {
+                ipAddress: data.trim()
+            };
+
+            resolve(external);
+        });
+    });
+
+    return promise;
 }
