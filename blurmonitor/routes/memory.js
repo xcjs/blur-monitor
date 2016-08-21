955901dce55c7b8c81bcbb57e10c589dca783a57
Zackary Lowery <zlowery@xcjs.com>
2016-08-21 17:37:19 -0400

    Corrected the memory API for newer versions of free.

--- blurmonitor/routes/memory.js
+++ blurmonitor/routes/memory.js
@@ -29,6 +29,7 @@ function getMemoryStats() {
         free.stdout.on('data', function (data) {
             var lines = data.toString().split(/\n/g),
                 line1 = lines[1].split(/\s+/),
+                line2 = lines[2].split(/\s+/),
                 line3 = lines[3].split(/\s+/),
                 total = parseInt(line1[1], 10),
                 free = parseInt(line1[3], 10),
@@ -36,9 +37,10 @@ function getMemoryStats() {
                 cached = parseInt(line1[6], 10),
                 actualFree = free + buffers + cached,
                 actualUsed = total - actualFree,
-                swapTotal = parseInt(line3[1], 10),
-                swapUsed = parseInt(line3[2], 10),
-                swapFree = parseInt(line3[3], 10),
+                // free versions 3.3.10 and above don't display -/+ buffers/cache line.
+                swapTotal = line3.length > 1 ? parseInt(line3[1], 10) : parseInt(line2[1], 10),
+                swapUsed = line3.length > 1 ? parseInt(line3[2], 10) : parseInt(line2[2], 10),
+                swapFree = line3.length > 1 ? parseInt(line3[3], 10) : parseInt(line2[3], 10),
                 memory = {
                     total: total,
                     used: parseInt(line1[2], 10),
