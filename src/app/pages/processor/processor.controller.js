beee354ef15d50beb0175d7ae78953013b320bd5
Zackary Lowery <zlowery@xcjs.com>
2016-08-29 19:43:13 -0400

    Processor data will load immediately with the controller.

--- src/app/pages/processor/processor.controller.js
+++ src/app/pages/processor/processor.controller.js
@@ -36,6 +36,7 @@
 
             vm.interval = null;
 
+            getProcessor();
             vm.interval = $interval(function() {
                 getProcessor();
             }, refreshInterval);
