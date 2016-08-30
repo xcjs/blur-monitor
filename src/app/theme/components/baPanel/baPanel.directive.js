c3d56446fd5823afa471add8490df5fc60eaa281
Zackary Lowery <zlowery@xcjs.com>
2016-08-29 20:50:41 -0400

    Added ng-class support to the BlurAdmin Panel.

--- src/app/theme/components/baPanel/baPanel.directive.js
+++ src/app/theme/components/baPanel/baPanel.directive.js
@@ -15,7 +15,7 @@
   function baPanel(baPanel, baConfig) {
     return angular.extend({}, baPanel, {
       template: function(el, attrs) {
-        var res = '<div  class="panel ' + (baConfig.theme.blur ? 'panel-blur' : '') + ' full-invisible ' + (attrs.baPanelClass || '');
+        var res = '<div ng-class="' + attrs.ngClass + '" class="panel ' + (baConfig.theme.blur ? 'panel-blur' : '') + ' full-invisible ' + (attrs.baPanelClass || '');
         res += '" zoom-in ' + (baConfig.theme.blur ? 'ba-panel-blur' : '') + '>';
         res += baPanel.template(el, attrs);
         res += '</div>';
