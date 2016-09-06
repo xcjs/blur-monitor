a5196d0ef902b5ce248817ab3dcf9150761ec4aa
Zackary Lowery <zlowery@xcjs.com>
2016-09-06 19:43:19 -0400

    Refactored system logo image inclusion to pull from provided assets.

---
+++ blurmonitor/services/shell.js
@@ -0,0 +1,57 @@
+/*jslint node: true */
+/*jslint esversion: 6 */
+
+'use strict';
+
+var childProcess = require('child_process');
+
+var exec = childProcess.exec;
+var spawn = childProcess.spawn;
+
+module.exports = {
+    exec: execWrapper,
+    spawn: spawnWrapper
+};
+
+function execWrapper(command) {
+    var promise = new Promise(function(resolve, reject) {
+        exec(command, function(error, stdout, stderr) {
+            if (error) {
+                reject('Command "' + command + '" failed. Please check for earlier errors');
+                return;
+            }
+
+            if(stderr) {
+                console.error(stderr);
+            }
+
+            resolve(stdout);
+        });
+    });
+
+    return promise;
+}
+
+function spawnWrapper(command, args) {
+    var process = spawn(command, args);
+
+    var promise = new Promise(function(resolve, reject) {
+        process.stdout.setEncoding("utf8");
+
+        process.stdout.on('data', function(stdout) {
+            resolve(stdout);
+        });
+
+        process.stderr.on('data', function(stderr) {
+            console.error(stderr);
+        });
+
+        process.on('close', function(code) {
+            if (code !== 0) {
+                reject('Command "' + command + '" failed. Please check for earlier errors');
+            }
+        });
+    });
+
+    return promise;
+}
